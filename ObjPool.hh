b54d8761e4d72f3629a8f032d574e88e7d4a66c0
kf2luv <15207683889@163.com>
2024-12-08 03:53:50 +0000

    update New() and Delete()

--- ObjPool.hh
+++ ObjPool.hh
@@ -8,44 +8,70 @@ class ObjectPool {
    public:
     ObjectPool() : _memory(nullptr), _free_list(nullptr), _rest_mem(0) {}
 
-    T *New() {
+    template <typename... Args>
+    T *New(Args &&...args) {
         T *t;
         try {
             if (_free_list != nullptr) {  // free链表有闲置内存块 (pop_front)
                 void *next = *(void **)_free_list;
                 t = (T *)_free_list;
                 _free_list = next;
-                std::cout << "take from free list successed!" << std::endl;
             } else {
                 if (_rest_mem < OBJ_SIZE) {  // 剩余空间不足
                     _memory = (char *)malloc(MEM_SIZE);
                     if (_memory == nullptr) {
                         throw std::bad_alloc();
                     }
-                    std::cout << "扩容成功，大小为： " << MEM_SIZE << std::endl;
                     _rest_mem = MEM_SIZE;
                 }
                 // 取出大小为OBJ_SIZE的空间返回
                 t = (T *)_memory;
                 // 取出的大小
-                size_t take_size =
-                    OBJ_SIZE >= sizeof(void *) ? OBJ_SIZE : sizeof(void *);
+                size_t take_size = OBJ_SIZE >= sizeof(void *) ? OBJ_SIZE : sizeof(void *);
                 //_memory覆盖掉取出的空间
                 _memory += take_size;
                 // 更新剩余空间
                 _rest_mem -= OBJ_SIZE;
-
-                std::cout << "取出空间: " << take_size << std::endl;
-                std::cout << "剩余空间: " << _rest_mem << std::endl;
             }
         } catch (const std::exception &e) {
             std::cout << e.what() << std::endl;
         }
 
+        // placement new
+        T *obj_ptr = new (t) T(std::forward<Args>(args)...);
+        return obj_ptr;
+    }
+
+    T *New() {
+        T *t;
+        try {
+            if (_free_list != nullptr) { 
+                void *next = *(void **)_free_list;
+                t = (T *)_free_list;
+                _free_list = next;
+            } else {
+                if (_rest_mem < OBJ_SIZE) { 
+                    _memory = (char *)malloc(MEM_SIZE);
+                    if (_memory == nullptr) {
+                        throw std::bad_alloc();
+                    }
+                    _rest_mem = MEM_SIZE;
+                }
+                t = (T *)_memory;
+                size_t take_size = OBJ_SIZE >= sizeof(void *) ? OBJ_SIZE : sizeof(void *);
+                _memory += take_size;
+                _rest_mem -= OBJ_SIZE;
+            }
+        } catch (const std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+
+        new (t) T;
         return t;
     }
 
     void Delete(T *obj) {
+        obj->~T();
         // 用归还块的前4/8个字节存储next指针
         *(void **)obj = _free_list;
         _free_list = (void *)obj;
--- main.cc
+++ main.cc
@@ -1,26 +1,104 @@
 #include "ObjPool.hh"
+#include <vector>
+using std::cout;
+using std::endl;
 
-class TestType{
-public:
+class TestType {
+   public:
+    TestType() : a(1), b(2), c(3) {}
+
+    TestType(int x, int y, int z) : a(x), b(y), c(z) {}
+
+   public:
     int a;
     int b;
     int c;
 };
 
-int main(){
-    ObjectPool<TestType> objPool;
+struct TreeNode
+{
+	int _val;
+	TreeNode* _left;
+	TreeNode* _right;
 
-    TestType* tt = objPool.New();
-    tt->a = 1;
-    tt->b = 2;
-    tt->c = 3;
+	TreeNode()
+		:_val(0)
+		, _left(nullptr)
+		, _right(nullptr)
+	{}
+};
 
-    TestType* tt1 = objPool.New();
-    TestType* tt2 = objPool.New();
-    TestType* tt3 = objPool.New();//扩容
+void TestObjectPool()
+{
+	// 申请释放的轮次
+	const size_t Rounds = 5;
 
-    objPool.Delete(tt);
-    TestType* tt4 = objPool.New();
+	// 每轮申请释放多少次
+	const size_t N = 1000000;
+
+	std::vector<TreeNode*> v1;
+	v1.reserve(N);
+
+	size_t begin1 = clock();
+	for (size_t j = 0; j < Rounds; ++j)
+	{
+		for (int i = 0; i < N; ++i)
+		{
+			v1.push_back(new TreeNode);
+		}
+		for (int i = 0; i < N; ++i)
+		{
+			delete v1[i];
+		}
+		v1.clear();
+	}
+
+	size_t end1 = clock();
+
+	std::vector<TreeNode*> v2;
+	v2.reserve(N);
+
+	ObjectPool<TreeNode> TNPool;
+	size_t begin2 = clock();
+	for (size_t j = 0; j < Rounds; ++j)
+	{
+		for (int i = 0; i < N; ++i)
+		{
+			v2.push_back(TNPool.New());
+		}
+		for (int i = 0; i < N; ++i)
+		{
+			TNPool.Delete(v2[i]);
+		}
+		v2.clear();
+	}
+	size_t end2 = clock();
+
+	cout << "new cost time:" << end1 - begin1 << endl;
+	cout << "object pool cost time:" << end2 - begin2 << endl;
+}
+
+int main() {
+    // ObjectPool<TestType> objPool;
+
+    // TestType* tt = objPool.New();
+    // std::cout << "a:" << tt->a << std::endl;
+    // std::cout << "b:" << tt->b << std::endl;
+    // std::cout << "c:" << tt->c << std::endl;
+
+    // TestType* tt1 = objPool.New(11, 22, 33);
+    // std::cout << "a:" << tt1->a << std::endl;
+    // std::cout << "b:" << tt1->b << std::endl;
+    // std::cout << "c:" << tt1->c << std::endl;
+
+    // TestType* tt1 = objPool.New();
+    // TestType* tt2 = objPool.New();
+    // TestType* tt3 = objPool.New();//扩容
+
+    // objPool.Delete(tt);
+    // TestType* tt4 = objPool.New();
+
+    TestObjectPool();
 
     return 0;
 }
